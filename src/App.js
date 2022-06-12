add67ef0d72bff7410111f016cb07384179e497a
Mike R <or4ngedeuce@gmail.com>
2022-06-12 17:55:15 -0500

    update name

--- src/App.js
+++ src/App.js
@@ -6,15 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Demon Cryptography!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Welcome to <h1>Demon Cryptography</h1>
+        </p>
       </header>
     </div>
   );
