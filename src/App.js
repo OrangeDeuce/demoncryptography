5676c069a0c49e0bf36d59e4cc0090ccc2b600d6
Mike R <or4ngedeuce@gmail.com>
2022-06-14 22:47:13 -0500

    logo change

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import demon from "./demon.png";
 import cryptography from "./cryptography.png";
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={demon} alt="demon logo" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={cryptography} alt="cryptography logo" />
-      </header>
-    </div>
+    <ChakraProvider>
+      <nav background="yellow">
+        <h1>This is the nav bar</h1>
+      </nav>
+      <div className="App">
+        <header className="App-header">
+          <img src={demon} alt="demon logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={cryptography} alt="cryptography logo" />
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
