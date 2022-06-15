fa09652cb53e420c70f828bff7529f351914780f
Mike R <or4ngedeuce@gmail.com>
2022-06-14 22:32:18 -0500

    change logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import demon from "./demon.png";
+import cryptography from "./cryptography.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={demon} />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to <h1>Demon Cryptography</h1>
-        </p>
+        <img src={cryptography} />
       </header>
     </div>
   );
